779fd6624ca3c3693a442aae9d0e2b824bb82748
Botwangsap <107746504+Botwangsap@users.noreply.github.com>
2022-08-21 00:17:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 global.owner = [
-  ['6285346545126', 'Acel', true],
+  ['6281998425782', 'Acel', true],
   // [number, dia creator/owner?, dia developer?]
   JSON.parse(fs.readFileSync('./src/owner.json'))
 ] // Put your number here
